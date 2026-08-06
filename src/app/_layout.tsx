e7d6f92c10447ffad6a817d0eb1f1493bdb0cb2b
Ben Ali Aboudou <158784500+Ben-TECHPlayer@users.noreply.github.com>
2026-08-06 23:32:10 +0200

    La barre de navigation est faite

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,1 +1,29 @@
-export default function TabLayout() {}
+import { createBottomTabNavigator } from "expo-router/build/react-navigation/bottom-tabs";
+// import { createNativeStackNavigator } from "expo-router/build/react-navigation/native-stack";
+// import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "../app/index";
+import DetailsProduit from "./DetailsProduit";
+import GestionProduits from "./GestionProduits";
+import TableauBord from "./TableauBord";
+
+// const Stack = createNativeStackNavigator();
+const menuNavigation = createBottomTabNavigator();
+
+export default function TabLayout() {
+  return (
+    // <NavigationContainer>
+    <menuNavigation.Navigator initialRouteName="Accueil">
+      <menuNavigation.Screen name="Accueil" component={HomeScreen} />
+      <menuNavigation.Screen
+        name="Détails de produits"
+        component={DetailsProduit}
+      />
+      <menuNavigation.Screen
+        name="Gestion de produits"
+        component={GestionProduits}
+      />
+      <menuNavigation.Screen name="Tableau de bord" component={TableauBord} />
+    </menuNavigation.Navigator>
+    // </NavigationContainer>
+  );
+}
