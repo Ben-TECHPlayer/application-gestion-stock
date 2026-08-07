e1703b3df0a5258cb9501dae4b0f099443d43b7e
Ben Ali Aboudou <158784500+Ben-TECHPlayer@users.noreply.github.com>
2026-08-07 15:33:05 +0200

    Sauvegarde de la branche principale

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,28 +2,28 @@ import { createBottomTabNavigator } from "expo-router/build/react-navigation/bot
 // import { createNativeStackNavigator } from "expo-router/build/react-navigation/native-stack";
 // import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "../app/index";
-// import DetailsProduit from "./DetailsProduit";
-// import GestionProduits from "./GestionProduits";
-// import TableauBord from "./TableauBord";
+// import DetailsProduit from "../app/detailsProduit";
+import GestionProduits from "../app/gestionProduits";
+// import TableauBord from "../app/tableauBord";
 
 // const Stack = createNativeStackNavigator();
 const menuNavigation = createBottomTabNavigator();
 
 export default function TabLayout() {
   return (
-    // <NavigationContainer>
     <menuNavigation.Navigator initialRouteName="Accueil">
       <menuNavigation.Screen name="Accueil" component={HomeScreen} />
-      {/* <menuNavigation.Screen
-        name="Détails de produits"
-        component={DetailsProduit}
-      />
+
       <menuNavigation.Screen
         name="Gestion de produits"
         component={GestionProduits}
       />
+
+      {/* <menuNavigation.Screen
+        name="Détails de produits"
+        component={DetailsProduit}
+      />
       <menuNavigation.Screen name="Tableau de bord" component={TableauBord} /> */}
     </menuNavigation.Navigator>
-    // </NavigationContainer>
   );
 }
